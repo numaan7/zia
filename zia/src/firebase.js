741c3166600f7109ed85a26a963b9c654857e9ca
Mohammed Noman <numaanmohammed996@gmail.com>
2025-06-10 08:49:59 +0530

    Update firebase.js

--- zia/src/firebase.js
+++ zia/src/firebase.js
@@ -7,13 +7,7 @@ import { getAuth } from 'firebase/auth';
 // For Firebase JS SDK v9-compat and later, measurementId is optional
 const firebaseConfig = {
   // Replace these with your actual Firebase config values
-   apiKey: "AIzaSyCLjRtggYgQt7IKHLrlx8gnJnTi87rGVdA",
-  authDomain: "sample-c07f9.firebaseapp.com",
-  projectId: "sample-c07f9",
-  storageBucket: "sample-c07f9.firebasestorage.app",
-  messagingSenderId: "711415598109",
-  appId: "1:711415598109:web:1042e597d4e9121e290af4",
-  measurementId: "G-B0NKDPW3DF"
+
 };
 
 // Initialize Firebase
